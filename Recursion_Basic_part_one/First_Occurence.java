b2e57f3cacf9d3214ae25b83d1b58d4ff3148f06
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-12-16 10:40:16 +0000

    First Occurence

---
+++ Recursion_Basic_part_one/First_Occurence.java
@@ -0,0 +1,16 @@
+public class First_Occurence {
+    public static int fistOccurence(int arr[],int key, int i){
+        if(i==arr.length){
+            return -1;
+        }
+        if(arr[i]==key){
+            return 1;
+        }
+        return fistOccurence(arr, key, i+1);
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7,8};
+        int key = 6;
+        System.out.println(fistOccurence(arr, key, 0));
+    }
+}
